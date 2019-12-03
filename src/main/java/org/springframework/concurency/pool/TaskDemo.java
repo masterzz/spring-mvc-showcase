17080310e29f0099f716dc6d28d08540d1c97c50
zhubr11 <zhubr11@chinaunicom.cn>
2019-12-03 16:17:18 +0800

    线程池

---
+++ src/main/java/org/springframework/concurency/pool/TaskDemo.java
@@ -0,0 +1,17 @@
+package org.springframework.concurency.pool;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class TaskDemo implements Runnable{
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+" is running");
+        try {
+            TimeUnit.SECONDS.sleep(new Random().nextInt(10));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
